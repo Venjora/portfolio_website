5d669480bea1991d4d47a5285f7b78b4481878fd
Venjora <ravenjohn3003@gmail.com>
2022-09-15 03:36:40 -0700

    creating a hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function () {
-  $(".header__menu").click(function () {
-    $(this).toggleClass("is-active");
-  });
+const hamburger = document.querySelector("#hamburger");
+const header = document.querySelector(".header");
+
+hamburger.addEventListener("click", function () {
+  console.log("open sesame");
+  if (header.classList.contains("is-active")) {
+    header.classList.remove("is-active");
+  } else {
+    header.classList.add("is-active");
+  }
 });
+
+// jQuery
+// $(document).ready(function () {
+//   $("#hamburger").click(function () {
+//     $(this).toggleClass("is-active");
+//   });
+// });
