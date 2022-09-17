a771d12dc212cddb0325043185025bdb47f2b56c
Venjora <ravenjohn3003@gmail.com>
2022-09-17 03:53:29 -0700

    Header design almost done!!

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,20 @@
 const hamburger = document.querySelector("#hamburger");
 const header = document.querySelector(".header");
-
+const overlay = document.querySelector(".overlay");
 hamburger.addEventListener("click", function () {
   console.log("open sesame");
+
   if (header.classList.contains("is-active")) {
     header.classList.remove("is-active");
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+    document.getElementById("sideNav").style.width = "0px";
   } else {
+    //open hamburger menu
     header.classList.add("is-active");
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+    document.getElementById("sideNav").style.width = "250px";
   }
 });
 
