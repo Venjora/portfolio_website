948a072bf3a214ada36133e626831cb116e1a1e5
Venjora <ravenjohn3003@gmail.com>
2022-09-21 01:34:30 -0700

    minor changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,19 @@
 const hamburger = document.querySelector("#hamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const sideNav = document.querySelector("#sideNav");
+const body = document.querySelector("#main");
+
 hamburger.addEventListener("click", function () {
   console.log("open sesame");
 
   if (header.classList.contains("is-active")) {
     header.classList.remove("is-active");
-    document.getElementById("sideNav").style.width = "0px";
+    sideNav.style.width = "0px";
   } else {
     //open hamburger menu
     header.classList.add("is-active");
-    document.getElementById("sideNav").style.width = "250px";
+    sideNav.style.width = "250px";
   }
 });
 
