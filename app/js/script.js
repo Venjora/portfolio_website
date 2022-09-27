1b59954409f826561109c54aab2349e0e1257552
Venjora <ravenjohn3003@gmail.com>
2022-09-27 00:50:37 -0700

    Creating the sub-heading section

--- app/js/script.js
+++ app/js/script.js
@@ -3,18 +3,23 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const sideNav = document.querySelector("#sideNav");
 const body = document.querySelector("#main");
+const activeToggle = document.querySelector(".header__activeToggle");
 
 hamburger.addEventListener("click", function () {
   console.log("open sesame");
 
-  if (header.classList.contains("is-active")) {
-    header.classList.remove("is-active");
-    sideNav.style.width = "0px";
-  } else {
-    //open hamburger menu
-    header.classList.add("is-active");
-    sideNav.style.width = "250px";
-  }
+  // if (header.classList.contains("is-active")) {
+  //   header.classList.remove("is-active");
+  //   sideNav.style.width = "0px";
+  // } else {
+  //   //open hamburger menu
+  header.classList.add("is-active");
+  sideNav.style.width = "250px";
+});
+
+activeToggle.addEventListener("click", function () {
+  header.classList.remove("is-active");
+  sideNav.style.width = "0px";
 });
 
 // jQuery
